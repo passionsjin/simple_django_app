28a8f5a25ce64dbde22c4a783541f183c3fa1485
Park youngjin <dudwls251@naver.com>
2020-06-22 10:25:38 +0900

    mv start.sh

--- simpleapp/start.sh
+++ simpleapp/start.sh
@@ -2,8 +2,8 @@
 
 set +e
 echo "==> Django setup, executing: migrate pro"
-python  manage.py migrate --settings=config.settings.production --fake-initial
+python  manage.py migrate --settings=simpleapp.settings.production --fake-initial
 echo "==> Django setup, executing: collectstatic"
-python  manage.py collectstatic --settings=config.settings.production --noinput -v 3
+python  manage.py collectstatic --settings=simpleapp.settings.production --noinput -v 3
 
 gunicorn -b 0.0.0.0:8000 --env DJANGO_SETTINGS_MODULE=simpleapp.settings.production simpleapp.wsgi:application
