e1a21b5c38549b910b9b5c71ac669b1ecfcc06e0
Park youngjin <dudwls251@naver.com>
2020-06-22 15:12:26 +0900

    Add mysql

--- simpleapp/simpleapp/settings/production.py
+++ simpleapp/simpleapp/settings/production.py
@@ -2,3 +2,14 @@ from .base import *
 
 
 INSTALLED_APPS += ['gunicorn']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['MYSQL_DATABASE'],
+        'USER': os.environ['MYSQL_USER'],
+        'PASSWORD': os.environ['MYSQL_PASSWORD'],
+        'HOST': os.environ['MYSQL_HOST'],
+        'PORT': os.environ['MYSQL_PORT'],
+    }
+}
