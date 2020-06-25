228997677838e16146f5695f1071fb5158293c21
Park youngjin <dudwls251@naver.com>
2020-06-25 11:52:03 +0900

    modify celery code

--- simpleapp/simpleapp/celery.py
+++ simpleapp/simpleapp/celery.py
@@ -11,6 +11,6 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
-@app.tasks(bind=True)
+@app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
